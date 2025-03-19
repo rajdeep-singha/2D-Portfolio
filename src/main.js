3758d45513c5827971627029b656c4baf7aa20d4
Rajdeep Singha <137903887+RajuC0der@users.noreply.github.com>
2025-03-19 22:04:42 +0530

    Fix asset paths

--- src/main.js
+++ src/main.js
@@ -2,21 +2,42 @@ import { dialogueData, scaleFactor } from "./constants";
 import { k } from "./kaboomCtx";
 import { displayDialouge, setCamScale } from "./utils";
 
-k.loadSprite("spritesheet", "spritesheet.png", {
-    sliceX: 39,
-    sliceY: 31,
-    anims:{
-      "idle-down":936,
-     "walk-down": { from: 936, to: 939, loop: true, speed: 8 },
+// k.loadSprite("spritesheet", "spritesheet.png", {
+//     sliceX: 39,
+//     sliceY: 31,
+//     anims:{
+//       "idle-down":936,
+//      "walk-down": { from: 936, to: 939, loop: true, speed: 8 },
+//     "idle-side": 975,
+//     "walk-side": { from: 975, to: 978, loop: true, speed: 8 },
+//     "idle-up": 1014,
+//     "walk-up": { from: 1014, to: 1017, loop: true, speed: 8 },
+//     }
+// });
+
+
+// k.loadSprite("map", "map.png");
+// loadSprite("map", `${import.meta.env.BASE_URL}map.png`);
+// loadSprite("spritesheet", `${import.meta.env.BASE_URL}spritesheet.png`);
+
+k.loadSprite("spritesheet", `${import.meta.env.BASE_URL}spritesheet.png`, {
+  sliceX: 39,
+  sliceY: 31,
+  anims: {
+    "idle-down": 936,
+    "walk-down": { from: 936, to: 939, loop: true, speed: 8 },
     "idle-side": 975,
     "walk-side": { from: 975, to: 978, loop: true, speed: 8 },
     "idle-up": 1014,
     "walk-up": { from: 1014, to: 1017, loop: true, speed: 8 },
-    }
+  }
 });
 
+k.loadSprite("map", `${import.meta.env.BASE_URL}map.png`);
 
-k.loadSprite("map", "map.png");
+
+
+k.loadJSON("/<your-repo-name>/map.json").then((data) => console.log(data));
 
 k.setBackground(k.Color.fromHex("#311047"));
 
